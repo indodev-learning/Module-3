fec5daccc3be050e9a19d56a2537a1311e0b73be
sibrha <msibrha@gmail.com>
2016-05-22 23:57:36 +0700

    with upload

--- js/main.js
+++ js/main.js
@@ -2,7 +2,9 @@ $(document).ready(function(){
 
 	var accessories = $('.accessories');
 	var dropPlace 	= $('#picture');
+	var dropImage 	= $('#drop');
 	var self 		= this;
+
 
 	// Draggable the accessories
 	accessories.draggable({
@@ -32,5 +34,51 @@ $(document).ready(function(){
 				grid: [150,150]
 			});
 		}
+	});
+
+	dropImage.on('dragenter', function (e){
+
+		e.stopPropagation();
+		e.preventDefault();
+
+		$(this).css('border', '2px solid #0B85A1');
+		$(this).css('background', 'limegreen');
+	});
+
+	dropImage.on('dragover', function (e) {
+
+		e.stopPropagation();
+		e.preventDefault();
+
+		$(this).css('background', 'limegreen');
+	});
+
+	dropImage.on('dragleave', function (e) {
+
+		e.stopPropagation();
+		e.preventDefault();
+
+		$(this).css('background', 'white');
+	});
+
+	dropImage.on('drop', function (e) {
+
+		$(this).css('border', '2px dotted #0B85A1');
+		$(this).css('color', 'white');
+		$(this).text('Dropped !');
+
+		e.preventDefault();
+
+		var image  = e.originalEvent.dataTransfer.files,
+			img    = image[0],
+			reader = new FileReader();
+
+		reader.addEventListener('load', function(e){
+
+
+			dropPlace.append("<img src='"+e.target.result+"'></img>");
+		});
+
+		reader.readAsDataURL(img);
 	});
 });
